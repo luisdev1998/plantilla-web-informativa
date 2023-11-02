1e9cacbf0f026aed77df43db1ee081f1e3125d2c
luiscarlosancheztapia <luis.dev1998@gmail.com>
2023-11-02 00:20:18 -0500

    aa

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './styles/Structure.css'
 import './styles/Desing.css'
+import './styles/Variables.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <App />
