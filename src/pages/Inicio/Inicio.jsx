ab5329f841660acde398ecc9593814ac2185e8b5
luiscarlosancheztapia <luis.dev1998@gmail.com>
2023-11-04 23:53:51 -0500

    a

--- src/pages/Inicio/Inicio.jsx
+++ src/pages/Inicio/Inicio.jsx
@@ -5,21 +5,38 @@ import Form from "../../components/Form/Form"
 export default function Inicio(){
     return(
         <>
-            <section className="flex column-direction vertical-center">
-                <div className="container-space container-space">
-                    
+            <section 
+                className="banner" 
+                style={{backgroundImage: "url('https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg')"}}>
+            </section>
+            <section className="flex horizontal-center">
+                <div className="container container-space">
+                    <h1 className="text-center">Conoce aquí <strong>Nuestros Proyectos</strong></h1>
+                    <div className="horizontal-col-2 gap-2em">
+                        <a className="card-img" style={{backgroundImage: "url('https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg')"}}></a>
+                        <a className="card-img" style={{backgroundImage: "url('https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg')"}}></a>
+                        <a className="card-img" style={{backgroundImage: "url('https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg')"}}></a>
+                        <a className="card-img" style={{backgroundImage: "url('https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg')"}}></a>
+                        <a className="card-img" style={{backgroundImage: "url('https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg')"}}></a>
+                        <a className="card-img" style={{backgroundImage: "url('https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg')"}}></a>
+                    </div>
                 </div>
             </section>
-            <section className="flex column-direction vertical-center">
-                <div className="container-space container-space">
-                    <h1 className="text-center">NUESTROS CLIENTES</h1>
+            <section className="flex horizontal-center">
+                <div className="container container-space">
+                    <h1 className="text-center">Aprovecha Nuestro <strong>¡35% DE DESCUENTO!</strong></h1>
+                    <div className="horizontal-col-2 gap-2em">
+                        <img src="https://storagecdnvivienda.blob.core.windows.net/webviviendahuphpprincipal/principal/uploads/banner-web19020x650--2023-09-01--16-42-18.jpg"/>
+                        <Card className="card-secondary flex column-direction gap-1em">
+                            <h3 className="text-color-secondary-1 text-center">Si realizas una compra al contado, llévate un súper descuento del 35%</h3>
+                            <video controls>
+                                <source src="https://www.dmaninmobiliaria.com/Inicio/Video.mp4" type="video/mp4"></source>
+                            </video>
+                        </Card>
+                    </div>
                 </div>
             </section>
-            <section className="flex column-direction vertical-center color-primary-1">
-                <div className="container-space container-space">
-                    <h1 className="text-center text-color-secondary-1">NUESTROS CLIENTES</h1>
-                </div>
-            </section>
+
             <section className="flex horizontal-center">
                 <div className="container container-space">
                     <h1 className="text-center">CONTACTO</h1>
